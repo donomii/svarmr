fa840b455f188f900067f5f38a1c56e9b8a1b4a7
Jeremy Price <donomii@gmail.com>
2016-05-14 12:57:00 +0200

    Something

--- sendMessage.go
+++ sendMessage.go
@@ -10,7 +10,7 @@ func main() {
     conn := svarmrgo.CliConnect()
     selector := os.Args[3]
     arg := os.Args[4]
-    m := svarmrgo.Mess{ Selector: selector, Arg: arg}
+    m := svarmrgo.Message{ Selector: selector, Arg: arg}
     fmt.Printf("%v\r\n", m)
     out, _ := json.Marshal(m)
     fmt.Printf("%s\r\n", out)
