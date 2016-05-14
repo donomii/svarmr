dff98ba93b703d2c493ffe6fcaa0afbec91db36c
Donomii <donomii@gmail.com>
2016-05-14 20:00:45 +0000

    Regualarly send a message to signal this computer is still alive

---
+++ heartbeat.go
@@ -0,0 +1,20 @@
+package main
+import (
+    "fmt"
+    "os"
+    "encoding/json"
+    "github.com/donomii/svarmrgo"
+    "time"
+)
+
+func main() {
+    conn := svarmrgo.CliConnect()
+    arg := os.Args[3]
+    m := svarmrgo.Message{ Selector: "heartbeat", Arg: arg}
+    out, _ := json.Marshal(m)
+    fmt.Printf("%s\r\n", out)
+    for {
+	    fmt.Fprintf(conn, fmt.Sprintf("%s\r\n", out))
+	    time.Sleep(1000 * time.Millisecond)
+	}
+}
