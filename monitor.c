6e164c4cdcb4edf5b2d89775566a050496e35876
donomii <donomii@gmail.com>
2016-05-16 05:48:01 +0900

    Initial import

---
+++ monitor.c
@@ -0,0 +1,224 @@
+/*
+** client.c -- a stream socket client demo
+Based on Beej's public domain example at http://beej.us/guide/bgnet/output/html/multipage/clientserver.html#simpleclient
+
+Compile with gcc monitor.c -Os -flto -lWs2_32
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+
+
+
+#ifdef _WIN32
+  /* See http://stackoverflow.com/questions/12765743/getaddrinfo-on-win32 */
+  #ifndef _WIN32_WINNT
+    #define _WIN32_WINNT 0x0501  /* Windows XP. */
+  #endif
+  #include <winsock2.h>
+  #include <Ws2tcpip.h>
+#else
+  /* Assume that any non-Windows platform uses POSIX-style sockets instead. */
+  #include <sys/socket.h>
+  #include <arpa/inet.h>
+  #include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
+  #include <unistd.h> /* Needed for close() */
+  #include <netinet/in.h>
+  #include <arpa/inet.h>
+
+#endif
+
+#include "../../udp/json-parser/json.h"
+#include "../../udp/json-parser/json.c"
+
+
+typedef struct {
+  char *Selector;
+  char *Arg;
+} Message;
+
+Message mess;
+char * str;
+
+
+#define PORT "4816" // the port client will be connecting to
+
+#define MAXDATASIZE 10000 // max number of bytes we can get at once
+
+
+static void print_depth_shift(int depth)
+{
+        int j;
+        for (j=0; j < depth; j++) {
+                printf(" ");
+        }
+}
+
+static void process_value(json_value* value, int depth);
+
+static void process_object(json_value* value, int depth)
+{
+        int length, x;
+        if (value == NULL) {
+                return;
+        }
+        length = value->u.object.length;
+        for (x = 0; x < length; x++) {
+                print_depth_shift(depth);
+                //printf("object[%d].name = %s\n", x, value->u.object.values[x].name);
+                process_value(value->u.object.values[x].value, depth+1);
+                if(strcmp(value->u.object.values[x].name, "Selector")==0) {
+                  mess.Selector = str;
+                }
+                if(strcmp(value->u.object.values[x].name, "Arg")==0) {
+                  mess.Arg = str;
+                }
+        }
+}
+
+static void process_array(json_value* value, int depth)
+{
+        int length, x;
+        if (value == NULL) {
+                return;
+        }
+        length = value->u.array.length;
+        printf("array\n");
+        for (x = 0; x < length; x++) {
+                process_value(value->u.array.values[x], depth);
+        }
+}
+
+static void process_value(json_value* value, int depth)
+{
+        int j;
+        if (value == NULL) {
+                return;
+        }
+        if (value->type != json_object) {
+                print_depth_shift(depth);
+        }
+        switch (value->type) {
+                case json_none:
+                        printf("none\n");
+                        break;
+                case json_object:
+                        process_object(value, depth+1);
+                        break;
+                case json_array:
+                        process_array(value, depth+1);
+                        break;
+                case json_integer:
+                        printf("int: %10" PRId64 "\n", value->u.integer);
+                        break;
+                case json_double:
+                        printf("double: %f\n", value->u.dbl);
+                        break;
+                case json_string:
+                        //printf("string: %s\n", value->u.string.ptr);
+                        str = value->u.string.ptr;
+                        break;
+                case json_boolean:
+                        printf("bool: %d\n", value->u.boolean);
+                        break;
+        }
+}
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, numbytes;
+    char buf[MAXDATASIZE];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    if (argc != 3) {
+        fprintf(stderr,"usage: client hostname port\n");
+        exit(1);
+    }
+
+
+    #ifdef _WIN32
+       WSADATA wsa_data;
+       WSAStartup(MAKEWORD(1,1), &wsa_data);
+     #else
+       return 0;
+     #endif
+
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+
+    if ((rv = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+printf("Connecting\n");
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+            s, sizeof s);
+    printf("client: connecting to %s\n", s);
+
+    freeaddrinfo(servinfo); // all done with this structure
+    for(;;) {
+    if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+        perror("recv");
+        exit(1);
+    }
+
+    buf[numbytes] = '\0';
+
+    //printf("client: received '%s'\n",buf);
+    json_char* json = (json_char*)buf;
+
+        json_value* value = json_parse(json,numbytes);
+        if (value == NULL) {
+                //fprintf(stderr, "Unable to parse data\n");
+        } else {
+        process_value(value, 0);
+        printf("%s:%s\n", mess.Selector, mess.Arg);
+      }
+}
+    close(sockfd);
+
+    return 0;
+}
