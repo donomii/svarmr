63fc4a82dc5c2bd4944915a1ea67b7cf5e351f87
Jeremy Price <donomii@gmail.com>
2016-05-21 13:10:20 +0200

    rename

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ go build relay.go &
 go build volume.go &
 go build heartBeat.go &
 go build monitor.go &
-go build mdnsWatcher.go &
+go build svarmrMdnsWatcher.go &
 go build mDNSprocessor.go &
 go build svarmrMessage.go &
 go build moduleStarter.go &
--- startModules.sh
+++ startModules.sh
@@ -2,7 +2,7 @@
 export HOST=localhost
 export PORT=4816
 ./svarmrMessage $HOST $PORT start-module usernotify  &
-./svarmrMessage $HOST $PORT start-module mdnsWatcher  &
+./svarmrMessage $HOST $PORT start-module svarmrMdnsWatcher  &
 ./svarmrMessage $HOST $PORT start-module mDNSprocessor  &
 sleep 1
 ./svarmrMessage $HOST $PORT user-notify "All modules loaded"
