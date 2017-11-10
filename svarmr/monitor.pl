fe305a46e2eaf41569bcd7c654a7f20a1ba1c6be
donomii <donomii+github@gmail.com>
2017-11-10 01:13:55 +0000

    Add support for stdio pips communication

--- svarmr/monitor.pl
+++ svarmr/monitor.pl
@@ -1,9 +1,21 @@
+use strict;
 use IO::Socket::INET;
  use JSON; # imports encode_json, decode_json, to_json and from_json.
 
- $sock = new IO::Socket::INET (
-     PeerHost => '127.0.0.1',
-     PeerPort => '4816',
+my $method = shift;
+die "First arg must be the connection method.  Either server:port or 'pipes'" unless $method;
+
+if ($method eq 'pipes') {
+	
+while ( my $line = <> ) {
+    my $s = decode_json($line);
+    print "** ".$s->{Selector}." **\n\n".$s->{Arg}."\n\n\n";
+}
+	} else {
+		my ($server, $port) = split/:/, $method;
+$sock = new IO::Socket::INET (
+     PeerHost => $server,
+     PeerPort => $port,
      Proto => 'tcp',
      Reuse => 1
  );
@@ -15,3 +27,4 @@ while ( my $line = <$sock> ) {
 }
 
 
+}
