7e9df0569143b4fdd8983cf6b527386c7c8f7325
Jeremy Price <donomii@gmail.com>
2017-11-19 22:35:28 +0100

    Expand canvas to fit window

--- guimodules/heart.tcl
+++ guimodules/heart.tcl
@@ -11,8 +11,9 @@ proc dict2json {dictToEncode} {
 
 source theme.tcl
 
-canvas .c -width 200 -height 200 -bg $color3
- pack .c
- .c create polygon 100 55 75 33 35 45 20 100 100 170 100 170 180 100 165 45 125 33 100 55 100 55 -smooth true -fill $color2
+frame .f -background $containerBackground
+canvas .f.c -width 200 -height 200 -bg $color3
+ pack .f.c .f -fill both -expand true
+ .f.c create polygon 100 55 75 33 35 45 20 100 100 170 100 170 180 100 165 45 125 33 100 55 100 55 -smooth true -fill $color2
 
 puts stdout  [ dict2json [ dict create Selector ModuleStart Arg Heart ] ]
